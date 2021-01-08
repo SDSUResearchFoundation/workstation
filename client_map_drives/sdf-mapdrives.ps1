19ed8ca102683ba3dafd2ec7fd3b21ae373101ea
Pete Delaney <pdelaney@sdsu.edu>
2021-01-07 22:09:55 -0800

    Update sdf-mapdrives.ps1

--- client_map_drives/sdf-mapdrives.ps1
+++ client_map_drives/sdf-mapdrives.ps1
@@ -21,21 +21,21 @@ $PL1_Path = "\\sdf-docserv.id.sdsu.edu\PL1"
 $log_path_exists = Test-Path $log_path
 if (!$log_path_exists){
     new-item -itemtype "directory" -path $log_path
-    echo "Log path absent. Directory $log_path created." | out-file $log_file -Append
+    write-output "Log path absent. Directory $log_path created." | out-file $log_file -Append
 }
 
 
 # Rotate the log if needed
 if (((Get-ChildItem $log_file).Length/1KB) -ge $log_rotate_threshold_in_kb){
-    echo "File bigger than $log_rotate_threshold_in_kb KB - Rotating log" | out-file $log_file -Append
+    write-output "File bigger than $log_rotate_threshold_in_kb KB - Rotating log" | out-file $log_file -Append
     $datetime = Get-Date -uformat "%Y-%m-%d-%H%M"
     $newname = "$((Get-ChildItem $log_file).BaseName)-${datetime}.log_old"
     Rename-Item $log_file $newname
-    echo "Rotated file name is $log_path\$newname" | out-file $log_file -Append
+    write-output "Rotated file name is $log_path\$newname" | out-file $log_file -Append
 }
 
 # Yay we're starting!  (it's for the log)
-echo "=== Script begin $(get-date) ===" | out-file $log_file -Append
+write-output "=== Script begin $(get-date) ===" | out-file $log_file -Append
 
 # Instantiate the variables (no undefined plz)
 [boolean]$SDrive_Exist = $False
@@ -55,11 +55,11 @@ $LDrive_Path_Exist = Test-Path $PL1_Path
 # Check if the drive exists
 if ($SDrive_Exist){
     # If it exists, move to the next command
-    echo "S Drive Exists" | out-file $log_file -Append
+    write-output "S Drive Exists" | out-file $log_file -Append
 }
 elseif ($SDrive_Path_Exist) {
     # If it does not exist, check if the path exists and then map it
-    echo "S Drive absent. Mapping..." | out-file $log_file -Append
+    write-output "S Drive absent. Mapping..." | out-file $log_file -Append
     New-PSDrive -Name "S" -Root $Scans_Path -Persist -PSProvider "FileSystem" -Scope Global -Verbose | out-file $log_file -Append
     $shell = New-Object -ComObject Shell.Application
     $shell.NameSpace("S:").Self.Name = "Scans"
@@ -69,17 +69,17 @@ elseif ($SDrive_Path_Exist) {
 # Check if the drive exists
 if ($LDrive_Exist){
     # If it exists, move to the next command
-    echo "L Drive Exists" | out-file $log_file -Append
+    write-output "L Drive Exists" | out-file $log_file -Append
 }
 elseif ($LDrive_Path_Exist){
     # If it does not exist, check if the path exists and then map it
-    echo "L Drive absent. Mapping..." | out-file $log_file -Append
+    write-output "L Drive absent. Mapping..." | out-file $log_file -Append
     New-PSDrive -Name "L" -Root $PL1_Path -Persist -PSProvider "FileSystem" -Scope Global -Verbose | out-file $log_file -Append
     $shell = New-Object -ComObject Shell.Application
     $shell.NameSpace("L:").Self.Name = "PL1"
 }
 
 # Finito. (for the log)
-echo "=== Script end $(get-date) ===" | out-file $log_file -Append
-echo "*******************************" | out-file $log_file -Append
+write-output "=== Script end $(get-date) ===" | out-file $log_file -Append
+write-output "*******************************" | out-file $log_file -Append
 
