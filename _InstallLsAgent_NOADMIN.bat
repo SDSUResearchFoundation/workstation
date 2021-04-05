ea51176e09092a1eb27b701bfb82ea496fbbad88
rfantest <73188613+rfantest@users.noreply.github.com>
2021-04-05 16:05:02 -0700

    Update _InstallLsAgent_NOADMIN.bat

--- _InstallLsAgent_NOADMIN.bat
+++ _InstallLsAgent_NOADMIN.bat
@@ -1,1 +1,1 @@
-"LsAgent-windows.exe" --server sdf-lansweeper.id.sdsu.edu --port 9524 --agentkey d50d7649-5950-4ca3-b6a8-e4edda0b675a --mode unattended
+"C:\_software\LsAgent-windows.exe" --server sdf-lansweeper.id.sdsu.edu --port 9524 --agentkey d50d7649-5950-4ca3-b6a8-e4edda0b675a --mode unattended
