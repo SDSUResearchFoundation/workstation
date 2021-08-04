f58f7d3dbc7c5259a0d50d50d03138f8c12fe62e
rfantest <73188613+rfantest@users.noreply.github.com>
2021-08-04 08:54:36 -0700

    Add files via upload

---
+++ zoho/wmi_install_script.bat
@@ -0,0 +1,13 @@
+@echo off
+C:\_software\zohotest\ZA_Access.exe /z"-silent -src deployment_tool" -s -f1"C:\_software\zohotest\ZohoAssistAgent.iss"
+set reg_cmd=reg query "hklm\software\zoho corp\zoho assist unattended agent\1.00.0001" /v "installation_status" /reg:32
+for /f "tokens=1-3" %%a in (' %reg_cmd% ') do ( 
+if not %%a == HKEY_LOCAL_MACHINE\software\zoho ( 
+if %%c == 0x1 exit 1000 
+if %%c == 0x2 exit 1001
+if %%c == 0x3 exit 1002
+if %%c == 0x4 exit 1003
+if %%c == 0x5 exit 1004
+if %%c == 0x6 exit 1005
+if %%c == 0x7 exit 1006 )
+)
